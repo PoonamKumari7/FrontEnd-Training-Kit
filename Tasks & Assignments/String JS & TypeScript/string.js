89cce8d26096e077fdf8aed75184302227423bd2
PoonamKumari7 <kumari.poonam3108@gmail.com>
2021-10-18 02:47:52 +0530

    Initial commit

---
+++ Tasks & Assignments/String JS & TypeScript/string.js
@@ -0,0 +1,23 @@
+//Finding length of string
+var var_name = new String("POONAM");
+var uname = new String("Hello Poonam, Welcome to typescript");
+console.log("Message: " + uname);
+console.log("Length: " + uname.length);
+//String Initialization  
+var str1 = 'Hello';
+var str2 = 'Poonam';
+//String Concatenation  
+console.log("Combined Result: " + str1.concat(str2));
+//String charAt  
+console.log("Character At 4: " + str2.charAt(4));
+
+string = 'AbhishekZankyani';
+//String indexOf  
+console.log("Index of Z: " + string.indexOf('Z'));
+//String replace  
+console.log("After Replacement: " + str1.replace('Hello', 'Welcome to'));
+//String uppercase  
+console.log("UpperCase: " + str2.toUpperCase());
+
+
+
